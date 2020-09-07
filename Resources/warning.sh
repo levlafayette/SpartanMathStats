038b9fcdc04c1a061d0d6d4cb09cf44e3596059a
Lev Lafayette <lev@levlafayette.com>
2020-09-07 10:36:32 +1000

    Core directory structure and README update

--- Resources/warning.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This is an abstract example of things that could do wrong!
-#SBATCH --output=/home/example/data/output_%j.out
-
-for for file in /home/example/data/* 
-	do
-        sbatch application ${file}
-	done
